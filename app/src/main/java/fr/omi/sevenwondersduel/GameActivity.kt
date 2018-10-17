79577b74b3d0a1fe60d1987527a9cbf76bf23d61
rfromi <romain.fromi@gmail.com>
2018-10-17 12:57:36 +0200

    Display available wonders when game starts

--- app/src/main/java/fr/omi/sevenwondersduel/GameActivity.kt
+++ app/src/main/java/fr/omi/sevenwondersduel/GameActivity.kt
@@ -3,8 +3,10 @@ package fr.omi.sevenwondersduel
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout.LayoutParams
 import android.support.constraint.ConstraintLayout.LayoutParams.MATCH_CONSTRAINT
+import android.support.constraint.Guideline
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import fr.omi.sevenwondersduel.Wonder.*
 import kotlinx.android.synthetic.main.activity_game.*
 
 
@@ -12,33 +14,86 @@ class GameActivity : AppCompatActivity() {
 
     val game = SevenWondersDuel()
 
+    private fun createHorizontalGuidelinesLayout(topGuideline: Guideline, bottomGuideline: Guideline, dpMargin: Int = 0): LayoutParams {
+        val params = LayoutParams(MATCH_CONSTRAINT, MATCH_CONSTRAINT)
+        params.topToBottom = topGuideline.id
+        params.bottomToTop = bottomGuideline.id
+        params.leftToLeft = layout.id
+        params.rightToRight = layout.id
+        val pxMargin = (dpMargin * resources.displayMetrics.density).toInt()
+        params.topMargin = pxMargin
+        params.bottomMargin = pxMargin
+        return params
+    }
+
+    private fun createImage(wonder: Wonder): ImageView {
+        val imageView = ImageView(this)
+        when (wonder) {
+            CIRCUS_MAXIMUS -> {
+                imageView.setImageResource(R.drawable.circus_maximus)
+                imageView.contentDescription = resources.getString(R.string.circus_maximus)
+            }
+            PIRAEUS -> {
+                imageView.setImageResource(R.drawable.piraeus)
+                imageView.contentDescription = resources.getString(R.string.piraeus)
+            }
+            THE_APPIAN_WAY -> {
+                imageView.setImageResource(R.drawable.the_appian_way)
+                imageView.contentDescription = resources.getString(R.string.the_appian_way)
+            }
+            THE_COLOSSUS -> {
+                imageView.setImageResource(R.drawable.the_colossus)
+                imageView.contentDescription = resources.getString(R.string.the_colossus)
+            }
+            THE_GREAT_LIBRARY -> {
+                imageView.setImageResource(R.drawable.the_great_library)
+                imageView.contentDescription = resources.getString(R.string.the_great_library)
+            }
+            THE_GREAT_LIGHTHOUSE -> {
+                imageView.setImageResource(R.drawable.the_great_lighthouse)
+                imageView.contentDescription = resources.getString(R.string.the_great_lighthouse)
+            }
+            THE_HANGING_GARDENS -> {
+                imageView.setImageResource(R.drawable.the_hanging_gardens)
+                imageView.contentDescription = resources.getString(R.string.the_hanging_gardens)
+            }
+            THE_MAUSOLEUM -> {
+                imageView.setImageResource(R.drawable.the_mausoleum)
+                imageView.contentDescription = resources.getString(R.string.the_mausoleum)
+            }
+            THE_PYRAMIDS -> {
+                imageView.setImageResource(R.drawable.the_pyramids)
+                imageView.contentDescription = resources.getString(R.string.the_pyramids)
+            }
+            THE_SPHINX -> {
+                imageView.setImageResource(R.drawable.the_sphinx)
+                imageView.contentDescription = resources.getString(R.string.the_sphinx)
+            }
+            THE_STATUE_OF_ZEUS -> {
+                imageView.setImageResource(R.drawable.the_statue_of_zeus)
+                imageView.contentDescription = resources.getString(R.string.the_statue_of_zeus)
+            }
+            THE_TEMPLE_OF_ARTEMIS -> {
+                imageView.setImageResource(R.drawable.the_temple_of_artemis)
+                imageView.contentDescription = resources.getString(R.string.the_temple_of_artemis)
+            }
+        }
+        return imageView
+    }
+
+    private fun displayAvailableWonder(wonder: Wonder, layoutParams: LayoutParams) {
+        val imageView = createImage(wonder)
+        imageView.layoutParams = layoutParams
+        layout.addView(imageView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
-        val imageView = ImageView(this)
-        imageView.setImageResource(R.drawable.the_great_library)
-        val params = LayoutParams(MATCH_CONSTRAINT, MATCH_CONSTRAINT)
-        imageView.layoutParams = params
-        params.topToBottom = guideline.id
-        params.bottomToTop = guideline2.id
-        params.leftToLeft = layout.id
-        params.rightToRight = layout.id
-        params.topMargin = (4 * resources.displayMetrics.density).toInt()
-        params.bottomMargin = (4 * resources.displayMetrics.density).toInt()
-        imageView.contentDescription = resources.getString(R.string.the_great_library)
-        layout.addView(imageView)
-
-        val imageView2 = ImageView(this)
-        imageView2.setImageResource(R.drawable.the_sphinx)
-        val params2 = LayoutParams(MATCH_CONSTRAINT, MATCH_CONSTRAINT)
-        imageView2.layoutParams = params2
-        params2.topToBottom = guideline2.id
-        params2.bottomToTop = guideline3.id
-        params2.leftToLeft = layout.id
-        params2.rightToRight = layout.id
-        params2.topMargin = (4 * resources.displayMetrics.density).toInt()
-        params2.bottomMargin = (4 * resources.displayMetrics.density).toInt()
-        imageView2.contentDescription = resources.getString(R.string.the_sphinx)
-        layout.addView(imageView2)
+        val wondersLayoutParams = listOf(createHorizontalGuidelinesLayout(boardBottom, line3, 4),
+                createHorizontalGuidelinesLayout(line3, line5, 4),
+                createHorizontalGuidelinesLayout(line5, line7, 4),
+                createHorizontalGuidelinesLayout(line7, bottomLine, 4))
+        game.wondersAvailable.zip(wondersLayoutParams).forEach { pair -> displayAvailableWonder(pair.first, pair.second) }
     }
 }
--- app/src/main/java/fr/omi/sevenwondersduel/Material.kt
+++ app/src/main/java/fr/omi/sevenwondersduel/Material.kt
@@ -9,14 +9,14 @@ import fr.omi.sevenwondersduel.Resource.Type.RAW_GOOD
 import fr.omi.sevenwondersduel.ScientificSymbol.*
 
 enum class Wonder(override val effects: List<Effect>, override val cost: Cost) : Construction {
-    THE_APPIAN_WAY(listOf(TakeCoins(3), OpponentLosesCoins(3), PlayAgain, VictoryPoints(3)), Cost(resources = mapOf(PAPYRUS to 1, CLAY to 2, STONE to 2))),
     CIRCUS_MAXIMUS(listOf(DestroyOpponentBuilding(MANUFACTURE), Shield(1), VictoryPoints(3)), Cost(resources = mapOf(GLASS to 1, WOOD to 1, STONE to 2))),
+    PIRAEUS(listOf(ProductionOfAny(MANUFACTURED_GOOD), PlayAgain, VictoryPoints(2)), Cost(resources = mapOf(CLAY to 1, STONE to 1, WOOD to 2))),
+    THE_APPIAN_WAY(listOf(TakeCoins(3), OpponentLosesCoins(3), PlayAgain, VictoryPoints(3)), Cost(resources = mapOf(PAPYRUS to 1, CLAY to 2, STONE to 2))),
     THE_COLOSSUS(listOf(Shield(2), VictoryPoints(3)), Cost(resources = mapOf(GLASS to 1, CLAY to 3))),
     THE_GREAT_LIBRARY(listOf(ChooseGreatLibraryProgress, VictoryPoints(4)), Cost(resources = mapOf(PAPYRUS to 1, GLASS to 1, WOOD to 3))),
     THE_GREAT_LIGHTHOUSE(listOf(ProductionOfAny(RAW_GOOD), VictoryPoints(4)), Cost(resources = mapOf(PAPYRUS to 2, STONE to 1, WOOD to 1))),
     THE_HANGING_GARDENS(listOf(TakeCoins(6), PlayAgain, VictoryPoints(3)), Cost(resources = mapOf(PAPYRUS to 1, GLASS to 1, WOOD to 2))),
     THE_MAUSOLEUM(listOf(BuildDiscarded, VictoryPoints(2)), Cost(resources = mapOf(PAPYRUS to 1, GLASS to 2, CLAY to 2))),
-    PIRAEUS(listOf(ProductionOfAny(MANUFACTURED_GOOD), PlayAgain, VictoryPoints(2)), Cost(resources = mapOf(CLAY to 1, STONE to 1, WOOD to 2))),
     THE_PYRAMIDS(listOf(VictoryPoints(9)), Cost(resources = mapOf(PAPYRUS to 1, STONE to 3))),
     THE_SPHINX(listOf(PlayAgain, VictoryPoints(6)), Cost(resources = mapOf(GLASS to 2, CLAY to 1, STONE to 1))),
     THE_STATUE_OF_ZEUS(listOf(DestroyOpponentBuilding(RAW_MATERIAL), Shield(1), VictoryPoints(3)), Cost(resources = mapOf(PAPYRUS to 2, CLAY to 1, WOOD to 1, STONE to 1))),
